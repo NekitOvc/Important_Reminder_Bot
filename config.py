8467ea10d9fa46b238dcd8747f87d98cc5a2714b
Nikita Ovchinnikov <n.ovchinnikov96@yandex.ru>
2022-11-13 16:25:20 +0300

    Important_Reminder_Bot

---
+++ config.py
@@ -0,0 +1,14 @@
+import emoji
+
+class ConfigTask:
+    TOKEN = '' #в ковычки вставить токен
+
+    welcome_text = emoji.emojize('Привет! :wave: Я бот!\
+    \nМоя задача - записать твои важные дела.\
+    \nНачнём? :wink:\
+    \n\n:point_right:Нажми на /help, чтобы узнать о моих возможностях.', language='alias')
+
+    help_text = emoji.emojize(':one: /add - добавить задачу. Ввод команды осуществляется в формате "/add дата задача"\
+        \n:two: /show - показать все задачи на определенную дату. Ввод команды осуществляется в формате "/show дата"', language='alias')
+
+    no_task = emoji.emojize('Задач на выбранную дату нет :neutral_face:. Добавь новую задачу!')
